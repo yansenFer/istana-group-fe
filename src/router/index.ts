4e18f1d8179f8e133a4629d8a78e26cc9fb7cd62
Yansenfer <yansenferdinand6@gmail.com>
2025-11-07 13:01:01 +0700

    handle middleware auth

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import LoginAdminView from '@/views/LoginAdminView.vue'
+import Cookies from 'universal-cookie'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -18,12 +19,27 @@ const router = createRouter({
     {
       path: '/about',
       name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
       component: () => import('../views/AboutView.vue'),
     },
   ],
+})
+
+// Middleware Auth
+router.beforeEach((to, from, next) => {
+  const cookies = new Cookies(null, { path: '/' })
+  const token = cookies.get('auth')
+
+  if (to.name === 'home' && !token) {
+    console.log('trigger 1')
+    return router.replace({ path: '/login-admin' })
+  }
+
+  if (to.name === 'Login' && token) {
+    console.log('trigger 2')
+    return router.replace({ name: 'Home' })
+  }
+
+  next()
 })
 
 export default router
